1ec70cfe694721f2b56bc4298c39487b719bbae0
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-02-28 19:54:42 -0300

    Criando solução ProjetosAulas

---
+++ ProjetosAulas/Aula1_2802/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1_2802
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
