80ec8d91f186090104cb4e106073b7ff66a60cc0
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-02-28 21:59:18 -0300

    Adicionando a classe Endereco

---
+++ SistemaEntregas/Modelos/Endereco.cs
@@ -0,0 +1,13 @@
+﻿namespace Modelos
+{
+    class Endereco
+    {
+        public int EnderecoID { get; set; }
+
+        public string Rua { get; set; }
+
+        public int Numero { get; set; }
+
+        public string Complemento { get; set; }
+    }
+}
