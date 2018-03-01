43a32d73229059969c690de1ffa6bff70cb84813
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-02-28 21:31:51 -0300

    Criando propriedades da classe Pessoa

--- ProjetosAulas/Aula1_2802/Pessoa.cs
+++ ProjetosAulas/Aula1_2802/Pessoa.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Aula1_2802
+﻿namespace Aula1_2802
 {
     class Pessoa
     {
+        public int PessoaID { get; set; }
+
+        public string Nome { get; set; }
+
     }
 }
