d76aae1a6b3215dcc6312d674971621f516940b2
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-03-07 20:21:09 -0300

    Adicionando método SalvarCliente

--- SistemaEntregas/Controllers/ClienteController.cs
+++ SistemaEntregas/Controllers/ClienteController.cs
@@ -1,7 +1,12 @@
-﻿namespace Controllers
+﻿using Modelos;
+
+namespace Controllers
 {
     public class ClienteController
     {
-
+        public void SalvarCliente(Cliente cliente)
+        {
+            // TODO: Persistir os dados do cliente
+        }
     }
 }
