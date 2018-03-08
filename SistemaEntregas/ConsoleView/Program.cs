1ba603dcdae4707c4a022d17d52986bbd8266781
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-03-07 21:12:25 -0300

    Utilizando ClienteController para salvar o cliente cadastrado

--- SistemaEntregas/ConsoleView/Program.cs
+++ SistemaEntregas/ConsoleView/Program.cs
@@ -1,4 +1,5 @@
-﻿using Modelos;
+﻿using Controllers;
+using Modelos;
 using System;
 
 namespace ConsoleView
@@ -47,6 +48,10 @@ namespace ConsoleView
                 {
                     case OpcoesMenuPrincipal.CadastrarCliente:
                         Cliente c = CadastrarCliente();
+
+                        ClienteController cc = new ClienteController();
+                        cc.SalvarCliente(c);
+                        
                         ExibirDadosCliente(c);
                         break;
                     case OpcoesMenuPrincipal.PesquisarCliente:
