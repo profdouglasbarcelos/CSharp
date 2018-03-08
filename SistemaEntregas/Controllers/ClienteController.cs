7f41d61b3e9f8490051e3ce094dee06a391ca30d
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-03-07 21:11:47 -0300

    Criando  e instanciando a lista Clientes (Generics)

--- SistemaEntregas/Controllers/ClienteController.cs
+++ SistemaEntregas/Controllers/ClienteController.cs
@@ -1,12 +1,24 @@
 ﻿using Modelos;
+using System.Collections.Generic;
 
 namespace Controllers
 {
     public class ClienteController
     {
+        public List<Cliente> MeusClientes { get; set; }
+
+        public ClienteController()
+        {
+            MeusClientes = new List<Cliente>();
+        }
+
+
+
+
         public void SalvarCliente(Cliente cliente)
         {
             // TODO: Persistir os dados do cliente
+            MeusClientes.Add(cliente);
         }
     }
 }
