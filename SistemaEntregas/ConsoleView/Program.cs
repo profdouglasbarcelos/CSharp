6ba32511e26496f7b3dae66b953d6e931343fea4
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-03-12 21:37:18 -0300

    Criando processo de exclusao (view)

--- SistemaEntregas/ConsoleView/Program.cs
+++ SistemaEntregas/ConsoleView/Program.cs
@@ -61,6 +61,7 @@ namespace ConsoleView
                     case OpcoesMenuPrincipal.EditarCliente:
                         break;
                     case OpcoesMenuPrincipal.ExcluirCliente:
+                        ExcluirCliente();
                         break;
                     case OpcoesMenuPrincipal.LimparTela:
                         break;
@@ -72,6 +73,14 @@ namespace ConsoleView
                
             } while (opcaoDigitada != OpcoesMenuPrincipal.Sair);
             
+        }
+
+        private static void ExcluirCliente()
+        {
+            Console.WriteLine("Digite o id do cliente que deseja excluir: ");
+            int idCliente = int.Parse(Console.ReadLine());
+
+
         }
 
         // Metodos Cliente
