94132a47a8dd077d0b2d8532fb499d65a7628c23
profdouglasbarcelos <prof.douglasbarcelos@gmail.com>
2018-04-11 20:09:40 -0300

    Criando Contexto

---
+++ SistemaEntregas/Modelos/DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace Modelos.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("stringConn")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+    }
+}
