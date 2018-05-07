6f29b9e81986bc2f59ed8f20a306749333cd0741
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-07 11:22:04 -0300

    Testando primeiros comandos console

--- Aulas/Aula0705_Console/Program.cs
+++ Aulas/Aula0705_Console/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aula0705_Console
 {
@@ -10,6 +6,18 @@ namespace Aula0705_Console
     {
         static void Main(string[] args)
         {
+            int a = 10;
+
+            Console.WriteLine("Hello World");
+
+            Console.WriteLine("a: " + a);
+
+            if (Console.CapsLock)
+                Console.WriteLine("CapsLock ativado");
+            else
+                Console.WriteLine("CapsLock desativado");
+
+            Console.ReadKey();
         }
     }
 }
