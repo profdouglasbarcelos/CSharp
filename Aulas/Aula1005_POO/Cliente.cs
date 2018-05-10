89aa863f29f1c280ae4449efa4f475e59610b00b
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-10 10:33:56 -0300

    Migrando conteudo da classe Cliente para classe Pessoa

--- Aulas/Aula1005_POO/Cliente.cs
+++ Aulas/Aula1005_POO/Cliente.cs
@@ -2,41 +2,9 @@
 {
     class Cliente
     {
-        // Atributo
-        private string nome;
-
-        // Propriedades
-        public string Nome
-        {
-            get
-            {
-                return nome;
-            }
-
-            set
-            {
-                nome = value;
-            }
-        }
         
-        public string Cpf { get; set; }
 
 
-        // Construtores
-        public Cliente() // Construtor vazio
-        {
-
-        }
-
-        public Cliente(string nome)
-        {
-            Nome = nome;
-        }
-
-        public Cliente(string nome, string cpf)
-        {
-            Nome = nome;
-            Cpf = cpf;
-        }
+        
     }
 }
--- Aulas/Aula1005_POO/Pessoa.cs
+++ Aulas/Aula1005_POO/Pessoa.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Aula1005_POO
+﻿namespace Aula1005_POO
 {
     class Pessoa
     {
+        // Atributo
+        private string nome;
+
+        // Propriedades
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public string Cpf { get; set; }
+
+        // Construtores
+        public Pessoa() // Construtor vazio
+        {
+
+        }
+
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }
+
+        public Pessoa(string nome, string cpf)
+        {
+            Nome = nome;
+            Cpf = cpf;
+        }
     }
 }
