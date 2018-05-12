2b7d1d5910e26dba08a5dcc5475ff42f6883ef61
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-11 21:26:02 -0300

    Criando e utilizando atributos e propriedades

--- Aulas/Aula1105_POO/Aluno.cs
+++ Aulas/Aula1105_POO/Aluno.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Aula1105_POO
+﻿namespace Aula1105_POO
 {
     class Aluno
     {
+        // Atributos
+        private string nome;
+
+        //Propriedades
+        public string Nome
+        {
+            get => nome; // get { return nome; }
+            set => nome = value; // set { nome = value; }
+        }
+
+        public string Cpf { get; set; }
     }
 }
--- Aulas/Aula1105_POO/Program.cs
+++ Aulas/Aula1105_POO/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Aula1105_POO
+﻿using System;
+
+namespace Aula1105_POO
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Aluno a = new Aluno();
+
+            a.Nome = "Godofredo"; // set
+            a.Cpf = "12345678909";
+            Console.WriteLine("Aluno:" + a.Nome); // get
+
+            
         }
     }
 }
