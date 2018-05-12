e394e24eee9dfba95eca88213755ab9cbb108ef3
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-11 21:08:18 -0300

    Adicionando classe Aluno

--- Aulas/Aula1105_POO/Program.cs
+++ Aulas/Aula1105_POO/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Aula1105_POO
+﻿namespace Aula1105_POO
 {
     class Program
     {
