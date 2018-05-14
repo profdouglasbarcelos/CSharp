d8a06a2d14c8f7ca8a1805f313be2c91f0558ea8
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-14 08:53:38 -0300

    Implementando IMeioTransporte

---
+++ Aulas/Aula1405_Interfaces/Carro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula1405_Interfaces
+{
+    class Carro : IMeioTransporte
+    {
+        public int Velocidade { get; set; }
+
+        public Carro()
+        {
+            Velocidade = 0;
+        }
+
+        public void Acelerar()
+        {
+            Velocidade += 10;
+        }
+
+        public void Desacelerar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
