5d571fc50c90c715a749113635148b8fee9ac329
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-14 21:17:06 -0300

    Criando interface IMeioTransporte

---
+++ Aulas/Aula1405_Interfaces/IMeioTransporte.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1405_Interfaces
+{
+    interface IMeioTransporte
+    {
+        void Acelerar();
+        void Desacelerar();
+    }
+}
