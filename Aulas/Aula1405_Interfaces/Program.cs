58a15f947a0880c0e90bb00e6d4a33c9b49939d6
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-14 21:30:10 -0300

    Criando objeto do tipo Carro e utilizando implementação de IMeioTransporte

--- Aulas/Aula1405_Interfaces/Program.cs
+++ Aulas/Aula1405_Interfaces/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Aula1405_Interfaces
+﻿using System;
+
+namespace Aula1405_Interfaces
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Carro c = new Carro();
+
+            Console.WriteLine("Carro criado, velocidade inicial: " + c.Velocidade);
+            c.Acelerar();
+            c.Acelerar();
+            Console.WriteLine("Carro, velocidade atual: " + c.Velocidade);
+
+            Console.ReadKey();
+
         }
     }
 }
