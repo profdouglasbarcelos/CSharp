bf15e77d870abad67a4e6e0ce75b35bef28c9983
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-14 21:59:00 -0300

    Implementando injeção de dependência

--- Aulas/Aula1405_Interfaces/Program.cs
+++ Aulas/Aula1405_Interfaces/Program.cs
@@ -11,10 +11,20 @@ namespace Aula1405_Interfaces
             Console.WriteLine("Carro criado, velocidade inicial: " + c.Velocidade);
             c.Acelerar();
             c.Acelerar();
+
+            AcelerarBastante(c);
             Console.WriteLine("Carro, velocidade atual: " + c.Velocidade);
 
             Console.ReadKey();
 
+        }
+
+        static void AcelerarBastante(IMeioTransporte meioTransporte)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                meioTransporte.Acelerar();
+            }
         }
     }
 }
