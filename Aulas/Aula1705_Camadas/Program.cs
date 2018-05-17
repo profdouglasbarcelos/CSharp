8f21cfdf3df354fda3d2443c1f090ae055fcaffb
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-17 08:20:04 -0300

    Criando projeto Aula1705_Camadas (versão inicial)

---
+++ Aulas/Aula1705_Camadas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1705_Camadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
