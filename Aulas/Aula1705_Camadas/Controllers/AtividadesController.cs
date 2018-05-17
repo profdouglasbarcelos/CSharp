ef117b9805a9c8cd84c113b1dc3dec187afaefd7
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-17 08:46:36 -0300

    Adicionando using's para List e Atividade

--- Aulas/Aula1705_Camadas/Controllers/AtividadesController.cs
+++ Aulas/Aula1705_Camadas/Controllers/AtividadesController.cs
@@ -1,4 +1,7 @@
-﻿namespace Aula1705_Camadas.Controllers
+﻿using Aula1705_Camadas.Models;
+using System.Collections.Generic;
+
+namespace Aula1705_Camadas.Controllers
 {
     class AtividadesController
     {
