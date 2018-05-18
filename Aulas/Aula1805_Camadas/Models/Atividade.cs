6dc24455ca769371776dbdd807dabb2b502563a2
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-18 19:32:56 -0300

    Criando model Atividade

---
+++ Aulas/Aula1805_Camadas/Models/Atividade.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula1805_Camadas.Models
+{
+    class Atividade
+    {
+        public int AtividadeID { get; set; }
+
+        public string Nome { get; set; }
+
+        public bool Ativo { get; set; }
+
+    }
+}
