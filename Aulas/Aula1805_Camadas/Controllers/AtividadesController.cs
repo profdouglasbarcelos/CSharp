70dcaa34f4798b928fd7d291a28f7665b8b3f18d
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-18 20:03:36 -0300

    Criando CRUD em AtividadesController

--- Aulas/Aula1805_Camadas/Controllers/AtividadesController.cs
+++ Aulas/Aula1805_Camadas/Controllers/AtividadesController.cs
@@ -1,13 +1,41 @@
-﻿using System;
+﻿using Aula1805_Camadas.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aula1805_Camadas.Controllers
 {
     class AtividadesController
     {
+        List<Atividade> minhasAtividades = new List<Atividade>();
 
+        // Salvar
+        public void Salvar(Atividade atividade)
+        {
+            minhasAtividades.Add(atividade);
+        }
+
+        // Listar
+        public List<Atividade> Listar()
+        {
+            return minhasAtividades;
+        }
+
+        // Editar
+        public void Editar(Atividade atividadeAtualizada)
+        {
+            foreach (Atividade a in minhasAtividades)
+            {
+                if(a.AtividadeID == atividadeAtualizada.AtividadeID)
+                {
+                    a.Nome = atividadeAtualizada.Nome;
+                    a.Ativo = atividadeAtualizada.Ativo;
+                }
+            }
+        }
+
+        // Excluir
+        public void Excluir(Atividade atividade)
+        {
+            minhasAtividades.Remove(atividade);
+        }
     }
 }
