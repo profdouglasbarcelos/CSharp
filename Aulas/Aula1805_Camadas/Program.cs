50cbccbdaf772dfacf443081d7a55d163eeed27f
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-18 20:42:43 -0300

    Criando métodos em AtividadesView (incompleto)

--- Aulas/Aula1805_Camadas/Program.cs
+++ Aulas/Aula1805_Camadas/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Aula1805_Camadas
+﻿using Aula1805_Camadas.Views;
+
+namespace Aula1805_Camadas
 {
     class Program
     {
         static void Main(string[] args)
         {
+            AtividadesView av = new AtividadesView();
+            av.ExibirMenu();
         }
     }
 }
--- Aulas/Aula1805_Camadas/Views/AtividadesView.cs
+++ Aulas/Aula1805_Camadas/Views/AtividadesView.cs
@@ -1,7 +1,78 @@
-﻿namespace Aula1805_Camadas.Views
+﻿using Aula1805_Camadas.Controllers;
+using Aula1805_Camadas.Models;
+using System;
+
+namespace Aula1805_Camadas.Views
 {
     class AtividadesView
     {
+        AtividadesController ac = new AtividadesController();
 
+
+        public void ExibirMenu()
+        {
+            int opcao = 6;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("=====================================");
+                Console.WriteLine("= 1) Cadastrar Atividade =");
+                Console.WriteLine("= 2) Listar Atividades =");
+                Console.WriteLine("= 3) Editar Atividade =");
+                Console.WriteLine("= 4) Excluir Atividade ");
+                Console.WriteLine("= 6) -- Sair -- ");
+                Console.WriteLine("=====================================");
+
+                Console.Write("Digite sua opcao: ");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        CadastrarAtiviade();
+                        break;
+
+                    case 2:
+                        ExibirAtividadesCadastradas();
+                        break;
+
+                    case 3:
+                        EditarAtividadeCadastrada();
+                    default:
+                        break;
+                }
+
+                Console.ReadKey();
+            } while (opcao != 6);
+        }
+
+        private void EditarAtividadeCadastrada()
+        {
+            
+        }
+
+        private void ExibirAtividadesCadastradas()
+        {
+            Console.WriteLine("-- Exibindo atividades cadastradas -- ");
+            foreach (Atividade a in ac.Listar())
+            {
+                Console.WriteLine("- Dados Atividade - ");
+                Console.WriteLine(" Id: " + a.AtividadeID);
+                Console.WriteLine(" Nome: " + a.Nome);
+                Console.WriteLine(" Ativo: " + a.Ativo);
+                Console.WriteLine("--");
+            }
+            Console.WriteLine("-- Fim da lista --");
+        }
+
+        private void CadastrarAtiviade()
+        {
+            Atividade atividade = new Atividade();
+            Console.Write("Digite o nome da atividade: ");
+            atividade.Nome = Console.ReadLine();
+            atividade.Ativo = true;
+            ac.Salvar(atividade);
+        }
     }
 }
