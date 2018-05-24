919c71cb4dcf600a194586ebe2187d4f96b2c546
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-24 09:22:45 -0300

    Criando modelos do projeto Controle de Atividades

---
+++ ControleAtividades/Modelos/Atividade.cs
@@ -0,0 +1,18 @@
+﻿namespace Modelos
+{
+    public class Atividade
+    {
+        public int AtividadeID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public int CategoriaID { get; set; }
+
+        public int UsuarioID { get; set; }
+
+    }
+}
---
+++ ControleAtividades/Modelos/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace Modelos
+{
+    class Categoria
+    {
+        public int CategoriaID { get; set; }
+
+        public string Nome { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
