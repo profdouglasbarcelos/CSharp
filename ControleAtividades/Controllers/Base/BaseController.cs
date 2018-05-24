4396825e8438b5ae6e3d900f8f4e792246edbafa
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-24 11:42:34 -0300

    Criando classe genérica BaseController

---
+++ ControleAtividades/Controllers/Base/BaseController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Controllers.Base
+{
+    public class BaseController<T> : IBaseController<T> where T : class
+    {
+        public void Adicionar(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T BuscarPorID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> ListarPorNome(string nome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
