9b051e072cb21620c0cd2f68edb7945b615dd4cd
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-05-25 19:50:50 -0300

    Criando projeto Modelos e as modelos: Usuario, Categoria e Atividade.

---
+++ ControleAtividades/Modelos/Atividade.cs
@@ -0,0 +1,20 @@
+﻿namespace Modelos
+{
+    class Atividade
+    {
+        public int AtividadeID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int CategoriaID { get; set; }
+
+        public virtual Categoria _Categoria { get; set; }
+
+        public int UsuarioID { get; set; }
+
+        public virtual Usuario _Usuario { get; set; }
+
+    }
+}
---
+++ ControleAtividades/Modelos/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace Modelos
+{
+    public class Usuario
+    {
+        public int UsuarioID { get; set; }
+
+        public string Nome { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
