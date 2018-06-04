6da7a8730162e0f742210ce4d82cc98c3e440583
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-06-04 09:29:58 -0300

    Criando CRUD utilizando contexto

--- ControleAtividades/Controllers/AtividadesController.cs
+++ ControleAtividades/Controllers/AtividadesController.cs
@@ -1,6 +1,70 @@
-﻿namespace Controllers
+﻿using System;
+using System.Collections.Generic;
+using Controllers.Base;
+using Modelos;
+using Controllers.DAL;
+using System.Linq;
+
+namespace Controllers
 {
-    class AtividadesController
+    public class AtividadesController : IBaseController<Atividade>
     {
+        private Contexto contexto = new Contexto();
+
+        public void Adicionar(Atividade entity)
+        {
+            contexto.Atividades.Add(entity);
+            contexto.SaveChanges();
+        }
+
+        public void Atualizar(Atividade entity)
+        {
+            contexto.Entry(entity).State =
+                System.Data.Entity.EntityState.Modified;
+
+            contexto.SaveChanges();
+        }
+
+        public Atividade BuscarPorID(int id)
+        {
+            return contexto.Atividades.Find(id);
+        }
+
+        public void Excluir(int id)
+        {
+            Atividade a = BuscarPorID(id);
+
+            if (a != null)
+            {
+                // forma 1
+                contexto.Atividades.Remove(a);
+
+                // forma 2
+                //contexto.Entry(a).State = System.Data.Entity.EntityState.Deleted;
+
+
+                contexto.SaveChanges();
+            }
+
+
+        }
+
+        public IList<Atividade> ListarPorNome(string nome)
+        {
+            // LINQ
+            //var atividadesComNome = from a in contexto.Atividades
+            //            where a.Nome == nome
+            //            select a;
+
+            //return atividadesComNome.ToList();
+
+            // LAMBDA
+            return contexto.Atividades.Where(a => a.Nome == nome).ToList();
+        }
+
+        public IList<Atividade> ListarTodos()
+        {
+            return contexto.Atividades.ToList();
+        }
     }
 }
