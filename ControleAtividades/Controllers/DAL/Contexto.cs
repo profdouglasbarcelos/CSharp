567184063aa31022486c2d069b6b4c57a39d81b4
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-06-04 09:02:32 -0300

    Escrevendo código da classe Contexto

--- ControleAtividades/Controllers/DAL/Contexto.cs
+++ ControleAtividades/Controllers/DAL/Contexto.cs
@@ -1,6 +1,19 @@
-﻿namespace Controllers.DAL
+﻿using Modelos;
+using System.Data.Entity;
+
+namespace Controllers.DAL
 {
-    class Contexto
+    class Contexto : DbContext
     {
+        public Contexto() : base("strConn")
+        {
+            
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Atividade> Atividades { get; set; }
+
+        public DbSet<Categoria> Categorias { get; set; }
     }
 }
