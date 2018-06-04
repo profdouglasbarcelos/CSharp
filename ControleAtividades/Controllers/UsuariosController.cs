fa1b9ac9bd0ada9036b7b0d12097d300c2a74608
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-06-04 10:17:15 -0300

    Escrevendo métodos de UsuariosController

--- ControleAtividades/Controllers/UsuariosController.cs
+++ ControleAtividades/Controllers/UsuariosController.cs
@@ -1,10 +1,55 @@
-﻿using Modelos;
+﻿using Controllers.Base;
+using Modelos;
 using System.Collections.Generic;
+using System;
+using Controllers.DAL;
+using System.Linq;
 
 namespace Controllers
 {
-    class UsuariosController
+    public class UsuariosController : IBaseController<Usuario>
     {
-        private List<Usuario> listaUsuarios { get; set; }
+        private Contexto contexto = new Contexto();
+
+        public void Adicionar(Usuario entity)
+        {
+            contexto.Usuarios.Add(entity);
+            contexto.SaveChanges();
+        }
+
+        public void Atualizar(Usuario entity)
+        {
+            contexto.Entry(entity).State = 
+                System.Data.Entity.EntityState.Modified;
+
+            contexto.SaveChanges();
+        }
+
+        public Usuario BuscarPorID(int id)
+        {
+            return contexto.Usuarios.Find(id);
+        }
+
+        public void Excluir(int id)
+        {
+            Usuario u = BuscarPorID(id);
+
+            if(u != null)
+            {
+                contexto.Usuarios.Remove(u);
+                contexto.SaveChanges();
+            }
+        }
+
+        public IList<Usuario> ListarPorNome(string nome)
+        {
+            return contexto.Usuarios
+                .Where(u => u.Nome.ToLower() == nome.ToLower()).ToList();
+        }
+
+        public IList<Usuario> ListarTodos()
+        {
+            return contexto.Usuarios.ToList();
+        }
     }
 }
