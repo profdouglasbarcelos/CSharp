e3df76fab4e9431bd416221942471f48644c0b30
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-06-04 11:21:17 -0300

    Adicionando segunda tela e mostrando navegação.

--- ControleAtividades/WpfView/MainWindow.xaml.cs
+++ ControleAtividades/WpfView/MainWindow.xaml.cs
@@ -23,6 +23,36 @@ namespace WpfView
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void btnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            btnSalvar.Content = "bla bla bla";
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            btnSalvar.Visibility = Visibility.Hidden;
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            btnSalvar.Visibility = Visibility.Visible;
+        }
+
+        private void txtNome_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtNome.Text.Length > 3)
+                MessageBox.Show("Voce escreveu: " + txtNome.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroUsuario cadUsu = new CadastroUsuario();
+            cadUsu.Show();
+            //cadUsu.ShowDialog();
+            this.Close();
         }
     }
 }
