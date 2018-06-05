36f1ffb06125f426bf96244ecde7b71da970fa45
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-06-04 21:23:48 -0300

    Implementando métodos da classe UsuariosController

--- ControleAtividades/Controllers/UsuariosController.cs
+++ ControleAtividades/Controllers/UsuariosController.cs
@@ -1,39 +1,67 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Controllers.Base;
+using Controllers.DAL;
 using Modelos;
 
 namespace Controllers
 {
     public class UsuariosController : IBaseController<Usuario>
     {
+        private Contexto contexto = new Contexto();
+
         public void Adicionar(Usuario entity)
         {
-            throw new System.NotImplementedException();
+            contexto.Usuarios.Add(entity);
+            contexto.SaveChanges();
         }
 
         public Usuario BuscarPorID(int id)
         {
-            throw new System.NotImplementedException();
+            return contexto.Usuarios.Find(id);
         }
 
         public void Editar(Usuario entity)
         {
-            throw new System.NotImplementedException();
+            contexto.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
         }
 
         public void Excluir(int id)
         {
-            throw new System.NotImplementedException();
+            Usuario usu = BuscarPorID(id);
+
+            if(usu != null)
+            {
+                //1a forma
+                //contexto.Entry(usu).State = System.Data.Entity.EntityState.Deleted;
+
+                // 2a forma
+                contexto.Usuarios.Remove(usu);
+
+
+                contexto.SaveChanges();
+            }
+
         }
 
         public IList<Usuario> ListarPorNome(string nome)
         {
-            throw new System.NotImplementedException();
+            // LINQ
+            //var usuariosComNome = from usu in contexto.Usuarios
+            //                      where usu.Nome == nome
+            //                      select usu;
+
+            //return usuariosComNome.ToList();
+
+            // LAMBDA
+            return contexto.Usuarios.Where(usu => usu.Nome == nome).ToList();
+
         }
 
         public IList<Usuario> ListarTodos()
         {
-            throw new System.NotImplementedException();
+            return contexto.Usuarios.ToList();
         }
     }
 }
