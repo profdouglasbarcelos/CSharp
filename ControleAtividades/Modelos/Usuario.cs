ea708d6ba440a6f524327247949364c236ae0dc9
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-06-08 19:59:01 -0300

    Adicionando Data Annotations

--- ControleAtividades/Modelos/Usuario.cs
+++ ControleAtividades/Modelos/Usuario.cs
@@ -1,9 +1,14 @@
-﻿namespace Modelos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelos
 {
     public class Usuario
     {
+        [Key]
         public int UsuarioID { get; set; }
 
+        [Required] // Campo obrigatorio
+        [StringLength(30)] // tamanho maximo 30
         public string Nome { get; set; }
 
         public bool Ativo { get; set; }
