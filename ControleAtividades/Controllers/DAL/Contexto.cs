ba743583ce7a98538f5eab12f3b78980144b3e1c
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-06-18 19:49:08 -0300

    Alteracao da modelo Usuario, estratégias de inicializacao do banco

--- ControleAtividades/Controllers/DAL/Contexto.cs
+++ ControleAtividades/Controllers/DAL/Contexto.cs
@@ -7,6 +7,15 @@ namespace Controllers.DAL
     {
         public Contexto() : base("strConn")
         {
+            // Padrao (se nao existir base de dados, cria)
+            Database.SetInitializer(new CreateDatabaseIfNotExists<Contexto>());
+
+            // Apaga e recria a base toda vez que o projeto eh executado
+            Database.SetInitializer(new DropCreateDatabaseAlways<Contexto>());
+
+            // Apaga e recria a base de dados se houver alteracoes nas modelos
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Contexto>());
+
 
         }
 
--- ControleAtividades/Modelos/Usuario.cs
+++ ControleAtividades/Modelos/Usuario.cs
@@ -11,6 +11,9 @@ namespace Modelos
         [StringLength(30)] // tamanho maximo 30
         public string Nome { get; set; }
 
+        [Required, StringLength(11)]
+        public string Cpf { get; set; }
+
         public bool Ativo { get; set; }
     }
 }
