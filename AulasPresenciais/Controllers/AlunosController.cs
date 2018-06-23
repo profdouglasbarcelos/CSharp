451558578ca8af486444b0c83c68affaafc6d2ec
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-06-23 14:04:16 -0300

    Criando classe AlunosController

---
+++ AulasPresenciais/Controllers/AlunosController.cs
@@ -0,0 +1,20 @@
+﻿using Modelos;
+using System.Collections.Generic;
+
+namespace Controllers
+{
+    public class AlunosController
+    {
+        List<Aluno> tabelaAlunos = new List<Aluno>();
+
+        public void Inserir(Aluno a)
+        {
+            tabelaAlunos.Add(a);
+        }
+
+        public List<Aluno> ListarTodos()
+        {
+            return tabelaAlunos;
+        }
+    }
+}
