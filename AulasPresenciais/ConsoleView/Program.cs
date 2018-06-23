700646a56cf22a82d1aa014179c040e9e2eef894
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-06-23 11:30:44 -0300

    Criando objeto (Instanciando) Aluno

--- AulasPresenciais/ConsoleView/Program.cs
+++ AulasPresenciais/ConsoleView/Program.cs
@@ -8,7 +8,11 @@ namespace ConsoleView
     {
         static void Main(string[] args)
         {
-           
+            Aluno a = new Aluno();
+            a.Nome = "Godofredo"; //set
+            a.Matricula = 123; // set
+
+            Console.WriteLine("Aluno: " + a.Nome); // get
 
             Console.ReadKey();
         }
