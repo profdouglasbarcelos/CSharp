9b9529aa9852fc69950e2c0f38fd75a6ae184e95
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-07-07 09:05:55 -0300

    Criando classe Contexto (espelho do banco)

---
+++ AulasPresenciais/Controllers/DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Modelos;
+using System.Data.Entity;
+
+namespace Controllers.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("strConn") // base = (super do Java), "strConn" = nome da conexão com banco
+        {
+
+        }
+
+        public DbSet<Aluno> Alunos { get; set; } // tabela de alunos
+    }
+}
