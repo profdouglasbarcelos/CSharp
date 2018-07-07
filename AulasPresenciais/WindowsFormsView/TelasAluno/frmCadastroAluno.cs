1dfb42d80544cad4b32bf9421636bc1ec897b15d
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-07-07 14:44:22 -0300

    Adicionando Entity e string de conexão no projeto WindowsForms

--- AulasPresenciais/WindowsFormsView/TelasAluno/frmCadastroAluno.cs
+++ AulasPresenciais/WindowsFormsView/TelasAluno/frmCadastroAluno.cs
@@ -1,13 +1,7 @@
 ﻿using Modelos;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Controllers;
 
 namespace WindowsFormsView.TelasAluno
 {
@@ -31,7 +25,8 @@ namespace WindowsFormsView.TelasAluno
             novoAluno.Nome = txtNome.Text;
             novoAluno.Matricula = int.Parse(txtMatricula.Text);
 
-   
+            AlunosController alunosController = new AlunosController();
+            alunosController.Inserir(novoAluno);
         }
     }
 }
