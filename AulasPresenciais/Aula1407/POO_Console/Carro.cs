6a73d192a895ffbfc358a47f3f1fd32c962465c0
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-07-14 11:22:24 -0300

    Implementando interface IVeiculo

--- AulasPresenciais/Aula1407/POO_Console/Carro.cs
+++ AulasPresenciais/Aula1407/POO_Console/Carro.cs
@@ -1,6 +1,32 @@
-﻿namespace POO_Console
+﻿using System;
+
+namespace POO_Console
 {
-    class Carro
+    class Carro : IVeiculo
     {
+        private int _velocidade;
+        public int Velocidade
+        {
+            get
+            {
+                return _velocidade;
+            }
+
+            set
+            {
+                _velocidade = value;
+            }
+        }
+
+
+        public void Acelerar()
+        {
+            _velocidade = _velocidade + 10;
+        }
+
+        public void Frenar()
+        {
+            _velocidade = _velocidade - 7;
+        }
     }
 }
