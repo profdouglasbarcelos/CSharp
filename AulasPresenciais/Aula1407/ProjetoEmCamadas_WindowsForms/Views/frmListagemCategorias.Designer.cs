589e80ef6568b87401ca9958e72a1eafa471db66
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-07-14 16:24:25 -0300

    Criando colunas (via smart tag, designer)

--- AulasPresenciais/Aula1407/ProjetoEmCamadas_WindowsForms/Views/frmListagemCategorias.Designer.cs
+++ AulasPresenciais/Aula1407/ProjetoEmCamadas_WindowsForms/Views/frmListagemCategorias.Designer.cs
@@ -29,16 +29,39 @@
         private void InitializeComponent()
         {
             this.dgvCategorias = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCategorias)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvCategorias
             // 
+            this.dgvCategorias.AllowUserToAddRows = false;
+            this.dgvCategorias.AllowUserToDeleteRows = false;
             this.dgvCategorias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategorias.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Nome});
             this.dgvCategorias.Location = new System.Drawing.Point(13, 13);
             this.dgvCategorias.Name = "dgvCategorias";
+            this.dgvCategorias.ReadOnly = true;
             this.dgvCategorias.Size = new System.Drawing.Size(259, 236);
             this.dgvCategorias.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "Id";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Visible = false;
+            // 
+            // Nome
+            // 
+            this.Nome.DataPropertyName = "Nome";
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            this.Nome.ReadOnly = true;
             // 
             // frmListagemCategorias
             // 
@@ -57,5 +80,7 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgvCategorias;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
     }
 }
