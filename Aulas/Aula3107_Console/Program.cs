ed08c8d0a8447e21866b3439dfc00b83bb52d1f0
profdouglasbarcelos <prof.douglasbarcelos@gmail.com>
2018-07-31 20:28:08 -0300

    Hello World

--- Aulas/Aula3107_Console/Program.cs
+++ Aulas/Aula3107_Console/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aula3107_Console
 {
@@ -10,6 +6,8 @@ namespace Aula3107_Console
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
