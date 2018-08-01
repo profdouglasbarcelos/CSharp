3b0bd4dab585cf3e4f9ea936df76f41ed37817b1
profdouglasbarcelos <prof.douglasbarcelos@gmail.com>
2018-07-31 21:25:56 -0300

    Iteracao com usuario, convertendo string para int (int.Parse)

--- Aulas/Aula3107_Console/Program.cs
+++ Aulas/Aula3107_Console/Program.cs
@@ -4,9 +4,17 @@ namespace Aula3107_Console
 {
     class Program
     {
+        public static int Id { get; set; }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Digite um numero");
+            Id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Voce digitou: " + Id);
+
             Console.ReadKey();
         }
     }
