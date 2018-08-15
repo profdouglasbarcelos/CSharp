ca3d37e680c452f65488aa53b1df1d11d0d39efc
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-08-14 21:27:59 -0300

    Criando projeto Aula1408_EntityFramework1 (instalação e conceitos)

---
+++ Aulas/Aula1408_EntityFramework1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1408_EntityFramework1
+{
+    /* Conceitos
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * Instalando Entity Framework
+     * 
+     */
+
+
+    public class Class1
+    {
+    }
+}
