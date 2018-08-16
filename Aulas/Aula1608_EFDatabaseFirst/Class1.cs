c3a32f39fcc63a038b3ed193e29898acb4361c51
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-08-16 20:21:49 -0300

    Criando projeto DatabaseFirst

---
+++ Aulas/Aula1608_EFDatabaseFirst/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1608_EFDatabaseFirst
+{
+    public class Class1
+    {
+    }
+}
