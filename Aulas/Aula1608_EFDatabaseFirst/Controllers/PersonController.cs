65e1f8d818e662f36ec515704811ecae5d1a360b
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-08-16 22:01:17 -0300

    Criando CRUD (incompleto) utilizando contexto

--- Aulas/Aula1608_EFDatabaseFirst/Controllers/PersonController.cs
+++ Aulas/Aula1608_EFDatabaseFirst/Controllers/PersonController.cs
@@ -8,6 +8,35 @@ namespace Aula1608_EFDatabaseFirst.Controllers
 {
     class PersonController
     {
+        void Inserir(Person p)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            contexto.Person.Add(p);
+            contexto.SaveChanges();
+        }
 
+        List<Person> ListarTodosPerson()
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            return contexto.Person.ToList();
+        }
+
+        Person BuscarPorID(int id)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            return contexto.Person.Find(id);
+        }
+
+        void Excluir(int id)
+        {
+            Person pExcluir = BuscarPorID(id);
+
+            if(pExcluir != null)
+            {
+                AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+                contexto.Person.Remove(pExcluir);
+                contexto.SaveChanges();
+            }
+        }
     }
 }
