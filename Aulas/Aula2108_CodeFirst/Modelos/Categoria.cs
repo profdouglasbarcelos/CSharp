33ebcd988b7932be47497cb8ad691aa9d8cd0334
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-08-21 21:31:50 -0300

    Escrevendo estrutura da classe Categoria (modelo)

--- Aulas/Aula2108_CodeFirst/Modelos/Categoria.cs
+++ Aulas/Aula2108_CodeFirst/Modelos/Categoria.cs
@@ -1,7 +1,14 @@
-﻿namespace Aula2108_CodeFirst.Modelos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aula2108_CodeFirst.Modelos
 {
     public class Categoria
     {
+        [Key]
+        public int CategoriaID { get; set; }
 
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
     }
 }
