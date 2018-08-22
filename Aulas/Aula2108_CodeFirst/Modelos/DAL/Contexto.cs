cbe50bf26eacdf23a88d8e05e808c1e8b1db97d6
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-08-21 21:59:23 -0300

    Herdando DbContext (Entity)

--- Aulas/Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
+++ Aulas/Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
@@ -1,6 +1,8 @@
-﻿namespace Aula2108_CodeFirst.Modelos.DAL
+﻿using System.Data.Entity;
+
+namespace Aula2108_CodeFirst.Modelos.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
 
     }
