12dedc54ce16a25fdb91f12559a085a113d79fa3
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-08-21 21:52:44 -0300

    Relacionamento Categoria --> Produtos e Data Annotations (Required, StringLength)

--- Aulas/Aula2108_CodeFirst/Modelos/Categoria.cs
+++ Aulas/Aula2108_CodeFirst/Modelos/Categoria.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Aula2108_CodeFirst.Modelos
 {
@@ -7,8 +9,14 @@ namespace Aula2108_CodeFirst.Modelos
         [Key]
         public int CategoriaID { get; set; }
 
+        [Required]
+        [StringLength(30)]
         public string Nome { get; set; }
 
         public string Descricao { get; set; }
+
+        // Relacionamento Categoria <--> Produto
+
+        public virtual ICollection<Produto> _Produtos { get; set; }
     }
 }
