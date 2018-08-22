d85908bd7f6e767bffa49df64f57a8f17c8fde86
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-08-21 22:02:56 -0300

    Definindo as propriedades referentes as futuras tabelas (categorias e produtos)

--- Aulas/Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
+++ Aulas/Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
@@ -9,5 +9,8 @@ namespace Aula2108_CodeFirst.Modelos.DAL
                 
         }
 
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
     }
 }
