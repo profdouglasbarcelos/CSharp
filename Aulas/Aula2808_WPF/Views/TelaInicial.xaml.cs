ffc0c3f0e5168d0cc35db95456b89522fc1a9b23
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-08-28 21:13:39 -0300

    Criando TelaInicial (navegação inicial App.xaml)

---
+++ Aulas/Aula2808_WPF/Views/TelaInicial.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aula2808_WPF.Views
+{
+    /// <summary>
+    /// Lógica interna para TelaInicial.xaml
+    /// </summary>
+    public partial class TelaInicial : Window
+    {
+        public TelaInicial()
+        {
+            InitializeComponent();
+        }
+    }
+}
