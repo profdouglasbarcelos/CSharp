7c93f55414a9d4ddc4cbb563189668668a9f2a4d
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-09-06 19:36:48 -0300

    Adicionando classe Atividade (Model)

---
+++ Aula0609_MVVM/Aula0609_MVVM/Models/Atividade.cs
@@ -0,0 +1,13 @@
+﻿namespace Aula0609_MVVM.Models
+{
+    public class Atividade
+    {
+        public int AtividadeID { get; set; }
+
+        public string Titutlo { get; set; }
+
+        public string Descricao { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
