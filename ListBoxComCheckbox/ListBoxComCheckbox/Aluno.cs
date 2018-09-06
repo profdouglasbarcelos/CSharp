5e33e493aae17e2ddd38d0a7128253340e9e579d
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-09-06 11:33:54 -0300

    Criando Exemplo de ListBox utilizando Checkbox

---
+++ ListBoxComCheckbox/ListBoxComCheckbox/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListBoxComCheckbox
+{
+    public class Aluno
+    {
+        public int AlunoID { get; set; }
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+    }
+}
---
+++ ListBoxComCheckbox/ListBoxComCheckbox/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListBoxComCheckbox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            List<Aluno> alunos = new List<Aluno>();
+
+            // Criando uma lista ficticia para simular dados salvos no banco
+            for (int i = 0; i < 5; i++)
+            {
+                Aluno a = new Aluno();
+                a.Nome = txtNome.Text + i;
+                a.Matricula = "Matrícula: " + i.ToString();
+                alunos.Add(a);
+            }
+
+            lbxAlunos.ItemsSource = alunos;
+        }
+    }
+}
